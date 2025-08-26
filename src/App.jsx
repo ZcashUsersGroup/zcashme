e361f97e996fe3d5edacade9370b92544da6f2dd
james-eda <james@edaclinical.com>
2025-08-25 23:39:25 -0400

    Fix typo: use ZcashProfile instead of ZcasherProfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './index.css'
 import { Routes, Route} from 'react-router-dom'
-import ZcasherProfile from './ZcashProfile'
+import ZcashProfile from './ZcashProfile'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ZcasherProfile />} />
-      <Route path="/:id" element={<ZcasherProfile />} />
+      <Route path="/" element={<ZcashProfile />} />
+      <Route path="/:id" element={<ZcashProfile />} />
     </Routes>
   )
 }
